5bfeb332cf1fa8d74e72d2726a2202ad1cb12125
FraneSpralja <spralja.frane@gmail.com>
2022-01-31 23:59:30 -0300

    Fix JS workin with forEach

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,7 +1,5 @@
-
-document.getElementById('table__header').innerHTML = '<h1>Estadisticas centro medico ñuñoa</h1>';
-
-let pacientes = [
+//Presentar array con objetos de los pacientes
+let radiologia = [
     {
         hora: "11.00",
         especialista: "Ignacio Schulz",
@@ -39,24 +37,187 @@ let pacientes = [
     },
 ];
 
-let tabla = "<tr><th>Hora</th><th>Especialista</th><th>Paciente</th><th>RUT</th><th>Previsión</th></tr>";
+console.log(`ver array ${radiologia}`);
+console.log(`Este array mide ${radiologia.length}`);
 
-for(let i = 0; i < pacientes.length; i++){
-    tabla += `<tr>
-            <td>${pacientes[i].hora}</td>
-            <td>${pacientes[i].especialista}</td>
-            <td>${pacientes[i].paciente}</td>
-            <td>${pacientes[i].rut}</td>
-            <td>${pacientes[i].prevision}</td>
-            </tr>`;
-}
+let traumatologia = [
+    {
+        hora: "08.00",
+        especialista: "María Paz Altuzarra",
+        paciente: "Paula Sanchez",
+        rut: "15554774-5",
+        prevision: "fonasa",
+    },
+    {
+        hora: "10.00",
+        especialista: "Raul Araya",
+        paciente: "Angélica Rivas",
+        rut: "15444147-9",
+        prevision: "isapre",
+    },
+    {
+        hora: "10.30",
+        especialista: "María Arriagada",
+        paciente: "Ana Klapp",
+        rut: "17879423-9",
+        prevision: "isapre",
+    },
+    {
+        hora: "11.00",
+        especialista: "Alejandra Badilla",
+        paciente: "Felipe Mardones",
+        rut: "1547423-6",
+        prevision: "isapre",
+    },
+    {
+        hora: "11.30",
+        especialista: "Cecilia Budnik",
+        paciente: "Diego Marre",
+        rut: "16554741-k",
+        prevision: "fonasa",
+    },
+    {
+        hora: "12.00",
+        especialista: "Arturo Cavagnaro",
+        paciente: "Cecilia Mendez",
+        rut: "9747535-8",
+        prevision: "isapre",
+    },
+    {
+        hora: "12.30",
+        especialista: "Andrés Kanacri",
+        paciente: "Marcial Suazo",
+        rut: "11254785-5",
+        prevision: "isapre",
+    },
+]
 
-document.getElementById("cuerpo-tabla").innerHTML = `${tabla}`;
+console.log(`ver array ${traumatologia}`);
+console.log(`Este array mide ${traumatologia.length}`);
 
-function first_last(){
-    let first = pacientes[0].paciente;
-    let last = pacientes[4].paciente;
-    document.write(`<br><b>Primer paciente:</b> ${first} | <b>Último paciente:</b> ${last}`);
-}
 
-document.querySelector('.table__header').innerHTML = `${first_last()}`;
+let dental = [
+    {
+        hora: "08.30",
+        especialista: "Andrea Zuñiga",
+        paciente: "Marcel Retamal",
+        rut: "11123425-6",
+        prevision: "isapre",
+    },
+    {
+        hora: "11.00",
+        especialista: "Maria Pia Zuñiga",
+        paciente: "Ángel Muñoz",
+        rut: "9878789-2",
+        prevision: "isapre",
+    },
+    {
+        hora: "11.30",
+        especialista: "Scarlett Witting",
+        paciente: "Mario Kast",
+        rut: "7998789-5",
+        prevision: "fonasa",
+    },
+    {
+        hora: "13.00",
+        especialista: "Francisca von Teuber",
+        paciente: "Karin Fernandez",
+        rut: "18887662-k",
+        prevision: "fonasa",
+    },
+    {
+        hora: "13.30",
+        especialista: "Eduardo Viñuela",
+        paciente: "Hugo Sanchez",
+        rut: "17665461-4",
+        prevision: "fonasa",
+    },
+    {
+        hora: "14.00",
+        especialista: "Raquel Villaseca",
+        paciente: "Ana Sepulveda",
+        rut: "14441281-0",
+        prevision: "isapre",
+    },
+]
+
+console.log(`ver array ${dental}`);
+console.log(`Este array mide ${dental.length}`);
+
+const pacientesRadio = document.querySelector('.first_last-1');
+pacientesRadio.innerHTML = `Primera atención: ${radiologia[0].paciente} // ${radiologia[0].prevision} | Última atención: ${radiologia[4].paciente} // ${radiologia[4].prevision}`;
+
+const tableRadio = document.querySelector('.tabla-1');
+radiologia.forEach((reserva) =>{
+    tableRadio.innerHTML += `
+    <tr>
+    <td>${reserva.hora}</td>
+    <td>${reserva.especialista}</td>
+    <td>${reserva.paciente}</td>
+    <td>${reserva.rut}</td>
+    <td>${reserva.prevision}</td>
+    </tr>`
+});
+
+const pacientesTrauma = document.querySelector('.first_last-2');
+pacientesTrauma.innerHTML = `Primera atención: ${traumatologia[0].paciente} // ${traumatologia[0].prevision} | Última atención ${traumatologia[6].paciente} // ${traumatologia[6].prevision}`;
+
+const tableTrauma = document.querySelector('.tabla-2');
+traumatologia.forEach((reserva) =>{
+    tableTrauma.innerHTML += `
+    <tr>
+    <td>${reserva.hora}</td>
+    <td>${reserva.especialista}</td>
+    <td>${reserva.paciente}</td>
+    <td>${reserva.rut}</td>
+    <td>${reserva.prevision}</td>
+    </tr>`
+});
+
+const pacientesDental = document.querySelector('.first_last-3');
+pacientesDental.innerHTML = `Primera atención: ${dental[0].paciente} // ${dental[0].prevision} | Última atención ${dental[5].paciente} // ${dental[5].prevision}`;
+
+const tableDental = document.querySelector('.tabla-3');
+dental.forEach((reserva) =>{
+    tableDental.innerHTML += `
+    <tr>
+    <td>${reserva.hora}</td>
+    <td>${reserva.especialista}</td>
+    <td>${reserva.paciente}</td>
+    <td>${reserva.rut}</td>
+    <td>${reserva.prevision}</td>
+    </tr>`
+});
+
+// let tabla = "<tr><th>Hora</th><th>Especialista</th><th>Paciente</th><th>RUT</th><th>Previsión</th></tr>";
+
+// pacientes.forEach(function(paciente){
+//     document.getElementById('cuerpo-tabla').innerHTML =
+//             `<tr>
+//             <td>${paciente.hora}</td>
+//             <td>${paciente.especialista}</td>
+//             <td>${paciente.paciente}</td>
+//             <td>${paciente.rut}</td>
+//             <td>${paciente.prevision}</td>
+//             </tr>`;
+// });
+
+// for(let i = 0; i < pacientes.length; i++){
+//     tabla += `<tr>
+//             <td>${pacientes[i].hora}</td>
+//             <td>${pacientes[i].especialista}</td>
+//             <td>${pacientes[i].paciente}</td>
+//             <td>${pacientes[i].rut}</td>
+//             <td>${pacientes[i].prevision}</td>
+//             </tr>`;
+// }
+
+// document.getElementById("cuerpo-tabla").innerHTML = `${tabla}`;
+
+// function first_last(){
+//     let first = pacientes[0].paciente;
+//     let last = pacientes[4].paciente;
+//     document.write(`<br><b>Primer paciente:</b> ${first} | <b>Último paciente:</b> ${last}`);
+// }
+
+// document.querySelector('.table__header').innerHTML = `${first_last()}`;
